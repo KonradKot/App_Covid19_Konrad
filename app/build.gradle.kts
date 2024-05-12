a8963fefe9f7972a3aa20a47aadaf8a97f45b13c
KonIT7 <90872151+KonIT7@users.noreply.github.com>
2024-05-12 17:09:29 +0200

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,18 @@ android {
 }
 
 dependencies {
+    /*
+    Sekcja dependencies w pliku build.gradle.kts określa wszystkie biblioteki, których aplikacja
+    potrzebuje do działania. Biblioteki te mogą być bibliotekami standardowymi Androida,
+    bibliotekami innych firm lub bibliotekami utworzonymi przez Ciebie.
+    Na przykład, w powyższym pliku build.gradle.kts sekcja dependencies zawiera następujące biblioteki:
+     */
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.20")  // standardowa bibliotaka języka Kotlin
+    implementation("androidx.core:core-ktx:1.13.1") // biblioteka rozszerzeń dla podstawowych funkcji Androida
+    implementation("com.android.volley:volley:1.2.1") // biblioteka do obsługi API REST, żądań HTTP
+    implementation("androidx.appcompat:appcompat:1.6.1") // biblioteka zapewniająca obsługe starszych wersji Androida
+    implementation("com.google.android.material:material:1.12.0") // biblioteka zapewniająca komponenty Material Design
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4") // biblioteka zapewniająca obsługe constraint layout
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
@@ -59,6 +71,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/app_covid19_konrad/MainActivity.kt
+++ app/src/main/java/com/example/app_covid19_konrad/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity // dodałem stworzoną klasę, któą zimportowałem w dependencies
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -13,17 +14,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.app_covid19_konrad.ui.theme.App_Covid19_KonradTheme
 
-import com.example.myproject.Greeting
-
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val greeting = Greeting()
-        val greetingText = greeting.greet()
-
-        // Display the greeting text on the screen
     }
 }
