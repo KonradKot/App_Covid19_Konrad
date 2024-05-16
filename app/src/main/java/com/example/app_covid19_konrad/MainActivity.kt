be6071afef857298c0b4772a162e0b30fa6571e4
KonIT7 <90872151+KonIT7@users.noreply.github.com>
2024-05-16 23:35:28 +0200

    Initial commit

--- app/src/main/java/com/example/app_covid19_konrad/MainActivity.kt
+++ app/src/main/java/com/example/app_covid19_konrad/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.app_covid19_konrad
 
 import android.app.DownloadManager
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity // dodałem stworzoną klasę, któą zimportowałem w dependencies
@@ -13,6 +16,7 @@ import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import org.json.JSONException
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,16 +27,25 @@ class MainActivity : AppCompatActivity() {
     lateinit var countryCasesTV: TextView
     lateinit var countryRecoveredTV: TextView
     lateinit var countryDeathsTV: TextView
-    lateinit var countryNameTV: TextView
 
     lateinit var stateRV: RecyclerView
     lateinit var stateRVAdapter: StateRVAdapter
     lateinit var stateList: List<StateModel>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val resetButton: Button = findViewById(R.id.reset_button) //dodanie przycisku do resetu
+
+        resetButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+            finish()
+        }
 
 
         worlCasesTV = findViewById(R.id.idTVWorldCases)
@@ -45,6 +58,15 @@ class MainActivity : AppCompatActivity() {
 
         stateRV = findViewById(R.id.idRVStates)
         stateList = ArrayList<StateModel>()
+
+        findViewById<Button>(R.id.get_data_btn_reg).setOnClickListener {
+            getStateInfo()// kiedy klikne pobieraj sie informacje o
+        }
+        findViewById<Button>(R.id.get_data_btn_wor).setOnClickListener {
+            //getStateInfo()// kiedy klikne pobieraj sie informacje o
+        }
+
+
     }
 
     private fun getStateInfo() // tu podaje zrodło API
@@ -53,45 +75,45 @@ class MainActivity : AppCompatActivity() {
         val url = "https://api.rootnet.in/covid19-in/stats/latest"
         val queue = Volley.newRequestQueue(this@MainActivity)
         val request =
-            JsonObjectRequest(Request.Method.GET,url,null,{ response ->
-                                                          try { // tu pobieram konkretne dane z API, które chcę wyswietlić
-                                                              val dataObj = response.getJSONObject("data")
-                                                              val summaryObj = dataObj.getJSONObject("summary")
-                                                              val cases: Int = summaryObj.getInt("total")
-                                                              val deaths: Int = summaryObj.getInt("deaths")
-                                                              val recovered: Int = summaryObj.getInt("discharged")
+            JsonObjectRequest(Request.Method.GET, url, null, { response ->
+                try { // tu pobieram konkretne dane z API, które chcę wyswietlić
+                    val dataObj = response.getJSONObject("data")
+                    val summaryObj = dataObj.getJSONObject("summary")
+                    val cases: Int = summaryObj.getInt("total")
+                    val deaths: Int = summaryObj.getInt("deaths")
+                    val recovered: Int = summaryObj.getInt("discharged")
 
-                                                              countryCasesTV.text = cases.toString()
-                                                              countryRecoveredTV.text = recovered.toString()
-                                                              countryDeathsTV.text = deaths.toString()
+                    countryCasesTV.text = cases.toString()
+                    countryRecoveredTV.text = recovered.toString()
+                    countryDeathsTV.text = deaths.toString()
 
-                                                              val regionalArray = dataObj.getJSONObject("regional") // pobranie danych regionalnie
-                                                              for (i in 0 until regionalArray.length()) {
-                                                                  val regionalObj = regionalArray.getJSONObject(i.toString())
-                                                                  val stateModel = StateModel(
-                                                                      regionalObj.getString("loc"),
-                                                                      regionalObj.getInt("discharged"),
-                                                                      regionalObj.getInt("deaths"),
-                                                                      regionalObj.getInt("totalConfirmed")
-                                                                  )
-                                                                  stateList += stateModel
-                                                              }
-                                                              stateRVAdapter = StateRVAdapter(stateList)
-                                                              stateRV.layoutManager = LinearLayoutManager(this)
-                                                              stateRV.adapter = stateRVAdapter
+                    val regionalArray =
+                        dataObj.getJSONArray("regional") // pobranie danych regionalnie
+                    for (i in 0 until regionalArray.length()) {
+                        val regionalObj = regionalArray[i] as JSONObject // castowanie do JSONObject]
+                        val stateModel = StateModel(
+                            regionalObj.getString("loc"),
+                            regionalObj.getInt("discharged"),
+                            regionalObj.getInt("deaths"),
+                            regionalObj.getInt("totalConfirmed")
+                        )
+                        stateList += stateModel
+                    }
+                    stateRVAdapter = StateRVAdapter(stateList)
+                    stateRV.layoutManager = LinearLayoutManager(this)
+                    stateRV.adapter = stateRVAdapter
 
+                    Log.w("MainActivity", "onCreate:")
 
-                                                          }catch (e: JSONException)
-                                                          {
-                                                              e.printStackTrace()
+                } catch (e: JSONException) {
+                    e.printStackTrace()
 
-                                                          }
+                }
             }, { error ->
                 {
                     Toast.makeText(this@MainActivity, "Something went wrong", Toast.LENGTH_SHORT)
                 }
             })
-        queue.add(request)
     }
     /*
     private fun getWorldInfo() // podobnie jak powyżej uzupełnić funkcje (poszukuje odpowiedniego Api)
@@ -101,13 +123,6 @@ class MainActivity : AppCompatActivity() {
     }
      */
 
-    override fun onStop() {
-        super.onStop()
-        // Zapisz dane do bazy danych lub pliku
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        // Zwolnij zasoby, takie jak wątki lub połączenia sieciowe
-    }
+
 
 }
