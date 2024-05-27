769f119574ff759a69e3af76d2f23f0002714a63
KonIT7 <90872151+KonIT7@users.noreply.github.com>
2024-05-27 04:01:24 +0200

    Initial commit

--- app/src/main/java/com/example/app_covid19_konrad/MainActivity.kt
+++ app/src/main/java/com/example/app_covid19_konrad/MainActivity.kt
@@ -114,7 +114,7 @@ class MainActivity : AppCompatActivity() {
             // Start the ImageActivity
             startActivity(intent)
         }
-        val showQuizButton = findViewById<Button>(R.chart_button)
+        val showQuizButton = findViewById<Button>(R.id.chart_button)
         showQuizButton.setOnClickListener {
             // Create an intent to start the ImageActivity
             val intent = Intent(this,ChartActivity::class.java)
